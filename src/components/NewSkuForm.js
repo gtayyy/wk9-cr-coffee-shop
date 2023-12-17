c598d2dc6320e2bfb6a97d0d0edcbff74935d7a0
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-16 23:19:55 -0800

    NewSkuForm.js done

--- src/components/NewSkuForm.js
+++ src/components/NewSkuForm.js
@@ -1,6 +1,7 @@
 import React from "react";
-import PropTypes from "react";
+import PropTypes from 'prop-types';
 import GeneralForm from './GeneralForm';
+import { v4 } from 'uuid';
 
 function NewSkuForm(props) {
 	function handleNewSkuFormSubmission(e) {
@@ -8,20 +9,23 @@ function NewSkuForm(props) {
 		props.onNewSkuCreation({
 			name: e.target.name.value,
 			origin: e.target.origin.value,
+			roast: e.target.roast.value,
 			price: e.target.origin.value,
-			roast: e.target.roast.value
+			amount: e.target.amount.value,
+			id: v4()
 		});
 	}
+
 	return (
-		<>
+		<React.Fragment>
 			<GeneralForm
 				formSubmissionHandler={handleNewSkuFormSubmission}
-				buttonText="Submit" />
-		</>
+				buttonText="Add SKU" />
+		</React.Fragment>
 	);
 }
 
-NewSkuForm.PropTypes = {
+NewSkuForm.propTypes = {
 	onNewSkuCreation: PropTypes.func
 };
 
