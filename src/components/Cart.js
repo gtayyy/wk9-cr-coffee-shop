778661e727adf394fa894c738c70df89d7ffc9be
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-17 17:40:50 -0800

    Add Cart.js

---
+++ src/components/Cart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Cart({ cartItems, removeFromCart }) {
+	return (
+		<div>
+			<h2>Shopping Cart</h2>
+			<ul>
+				{cartItems.map((item) => (
+					<li key={item.id}>
+						{item.name} - {item.price}
+						<button onClick={() => removeFromCart(item.id)}>Remove</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default Cart;
