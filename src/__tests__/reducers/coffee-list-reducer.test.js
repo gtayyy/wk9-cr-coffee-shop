7a6a31125f0f3ae9f759f2f67bde14f47149d30f
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2024-01-01 13:51:03 -0800

    Add SKU test good fail

--- src/__tests__/reducers/coffee-list-reducer.test.js
+++ src/__tests__/reducers/coffee-list-reducer.test.js
@@ -1,7 +1,42 @@
 import coffeeListReducer from './../../reducers/coffee-list-reducer.js';
 
 describe('coffeeListReducer', () => {
+
+	let action;
+	const skuData = {
+		name: 'Double or Nothing',
+		origin: 'Canada Eh',
+		roast: 'Medium',
+		price: '27.99',
+		lbsOnHand: '130',
+		id: 1
+	}
+
 	test('Should return default state if there is no action type passed into the reducer', () => {
 		expect(coffeeListReducer({}, { type: null })).toEqual({});
 	});
+
+	test('Should successfully add a new coffee SKU to mainCoffeeList', () => {
+		const { name, origin, roast, price, lbsOnHand } = skuData;
+		action = {
+			type: 'ADD_SKU',
+			name: name,
+			origin: origin,
+			roast: roast,
+			price: price,
+			lbsOnHand: lbsOnHand,
+			id: id
+		};
+		expect(coffeeListReducer({}, action)).toEqual({
+			[id]: {
+				name: name,
+				origin: origin,
+				roast: roast,
+				price: price,
+				lbsOnHand: lbsOnHand,
+				id: id
+			}
+		});
+	});
+	
 });
