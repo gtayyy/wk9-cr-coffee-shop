c2be7b8b2bc2088aa7b1691052dafe527d9bb35a
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2024-01-01 17:45:34 -0800

    index reducer smoke tests pass

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from './../../reducers/index.js';
+import { legacy_createStore as createStore } from 'redux';
+import formVisibleReducer from './../../reducers/form-visible-reducer.js';
+import coffeeListReducer from './../../reducers/coffee-list-reducer.js';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -7,6 +12,14 @@ describe("rootReducer", () => {
 			mainCoffeeList: {},
 			formVisibleOnPage: false
 		});
+	});
+
+	test('Check that initial state of coffeeListReducer matches root reducer', () => {
+		expect(store.getState().mainCoffeeList).toEqual(coffeeListReducer(undefined, { type: null }));
+	});
+
+	test('Check that initial state of formVisibleReducer matches root reducer', () => {
+		expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
 	});
 
 });
