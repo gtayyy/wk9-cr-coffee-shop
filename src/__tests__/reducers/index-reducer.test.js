8a9d109dd74a0b3cee92118fd55271cc023f0d67
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2024-01-01 17:27:31 -0800

    index-reducer test written

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,12 @@
+import rootReducer from './../../reducers/index.js';
+
+describe("rootReducer", () => {
+
+	test('Should return default state if no action type is recognized', () => {
+		expect(rootReducer({}, { type: null })).toEqual({
+			mainCoffeeList: {},
+			formVisibleOnPage: false
+		});
+	});
+
+});
